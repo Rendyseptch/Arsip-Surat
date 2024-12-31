ed3b629077b2b08e50eebe7890c3eded8ff3b38e
Rendy Septian Pradana <126131312+Rendyseptch@users.noreply.github.com>
2024-12-31 14:34:08 +0700

    change form color

--- resources/views/arsip/create.blade.php
+++ resources/views/arsip/create.blade.php
@@ -7,64 +7,64 @@
         @csrf
         <div class=" relative flex flex-col space-y-4 m-6">
             <div class="mb-6">
-                <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+                <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 ">Nama</label>
                 <input type="text" id="nama" name="nama"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Nama" required />
             </div>
             <div class="mb-6">
-                <label for="nomor" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nomor
+                <label for="nomor" class="block mb-2 text-sm font-medium text-gray-900">Nomor
                 </label>
                 <input type="text" id="nomor" name="nomor"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Nomor" required />
             </div>
             <div class="mb-6">
-                <label for="jenis" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis Dinas
+                <label for="jenis" class="block mb-2 text-sm font-medium text-gray-900 ">Jenis Dinas
                 </label>
                 <select id="jenis" name="jenis"
-                    class="mb-2 text-dark bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-dark dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    class="mb-2 text-dark bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
                     <option selected>Jenis Dinas</option>
                     <option value="dinas luar">Dinas Luar</option>
                     <option value="dinas dalam">Dinas Dalam</option>
                 </select>
             </div>
             <div class="mb-6">
-                <label for="tanggal" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal
+                <label for="tanggal" class="block mb-2 text-sm font-medium text-gray-900 ">Tanggal
                 </label>
                 <input type="date" id="tanggal" name="tanggal"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="tanggal" required />
             </div>
             <div class="mb-6">
-                <label for="waktu" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Waktu
+                <label for="waktu" class="block mb-2 text-sm font-medium text-gray-900 ">Waktu
                 </label>
                 <input type="time" id="waktu" name="waktu"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="waktu" required />
             </div>
             <div class="mb-6">
-                <label for="alamat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat
+                <label for="alamat" class="block mb-2 text-sm font-medium text-gray-900 ">Alamat
                 </label>
                 <input type="text" id="alamat" name="alamat"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="alamat" required />
             </div>
             <div class="mb-6">
-                <label for="dokumen" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">dokumen
+                <label for="dokumen" class="block mb-2 text-sm font-medium text-gray-900 ">dokumen
                 </label>
                 <input type="file" id="dokumen" name="dokumen"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="dokumen" required />
             </div>
             <div class="mb-6">
-                <label for="keterangan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">keterangan
+                <label for="keterangan" class="block mb-2 text-sm font-medium text-gray-900 ">keterangan
                 </label>
                 <input type="text" id="keterangan" name="keterangan"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="keterangan" />
             </div>
-            <button type="submit" class="bg-blue-600 p-2 rounded-md text-white">submit</button>
+            <button type="submit" class="bg-blue-600 p-2 rounded-md text-gray-900">submit</button>
     </form>
     </div>
 
